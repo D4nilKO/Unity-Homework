061c70261205845d9c72d4c15b3e0252c16d58f0
Даниил Понкратенко <ponkratenko.danil@mail.ru>
2023-11-13 12:09:05 +0300

    Add Task (6) Generation of enemies on the level
    На уровне присутствует несколько точек спавна (создания врагов).
    
    Каждые две секунды в случайной точке спавна должен появляться новый враг. После этого он должен начать двигаться по прямой в том направлении, которое ему укажет спавнер.

---
+++ 3D Homework/Assets/Tasks/Enemy Spawner/EnemySpawner.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Tasks.Enemy_Spawner
+{
+    public class EnemySpawner : MonoBehaviour
+    {
+        [SerializeField] [Min(1)] private float _timeToSpawn = 1f;
+        [SerializeField] private Enemy _enemy;
+        [SerializeField] private List<Transform> _spawnPoints = new();
+        
+        private void Start()
+        {
+            StartCoroutine(Spawn());
+        }
+
+        private IEnumerator Spawn()
+        {
+            float time = 0;
+
+            while (time <= _timeToSpawn)
+            {
+                time += Time.deltaTime;
+                yield return null;
+            }
+
+            Enemy enemy = Instantiate(_enemy, GetRandomPoint().position, Quaternion.identity);
+            enemy.Init(GetRandomDirection());
+
+            StartCoroutine(Spawn());
+        }
+
+        private Vector3 GetRandomDirection()
+        {
+            return new Vector3(Random.Range(-1f, 1f), 0, Random.Range(-1f, 1f)).normalized; // ?
+        }
+
+        private Transform GetRandomPoint()
+        {
+            return _spawnPoints[Random.Range(0, _spawnPoints.Count)];
+        }
+    }
+}
