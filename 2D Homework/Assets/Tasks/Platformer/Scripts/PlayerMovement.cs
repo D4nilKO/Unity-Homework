bdd85c67646707fa69f08d652bc63c4c7c1e6f9d
Даниил Понкратенко <ponkratenko.danil@mail.ru>
2023-12-01 23:58:10 +0300

    Can Move and can Jump (for 1 time before landing)

--- 2D Homework/Assets/Tasks/Platformer/Scripts/PlayerMovement.cs
+++ 2D Homework/Assets/Tasks/Platformer/Scripts/PlayerMovement.cs
@@ -6,71 +6,51 @@ namespace Tasks.Platformer.Scripts
     public class PlayerMovement : MonoBehaviour
     {
         private const string MovingAxisName = "Horizontal";
-
+        
+[SerializeField] private Transform _groundChecker;
         [SerializeField] private float _speed = 3f;
-        [SerializeField] private float _jumpForce = 4f;
+        [SerializeField] private float _jumpForce = 15f;
 
-        [SerializeField] private ContactFilter2D _contactFilter;
-        
+        [SerializeField] private LayerMask _layerMask;
+
         private Rigidbody2D _rigidbody;
-        private RaycastHit2D[] _hits = new RaycastHit2D[1];
-        
-        [SerializeField]private bool _canJumping = true;
+
+        private float _rayLenght = 0.1f;
 
         private void Start()
         {
             _rigidbody = GetComponent<Rigidbody2D>();
         }
 
-        private void FixedUpdate()
+        private void Update()
         {
             Move();
-            Jump();
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                GroundChecking();
+            }
         }
 
         private void Move()
         {
-            Vector3 direction = GetDirection();
-            float distance = _speed * Time.deltaTime;
+            float directionX = Input.GetAxis(MovingAxisName);
+            _rigidbody.velocity = new Vector2(directionX * _speed, _rigidbody.velocity.y);
+        }
 
-            if (_rigidbody.Cast(direction, _contactFilter, _hits, distance) == 0)
+        private void GroundChecking()
+        {
+            RaycastHit2D hit = Physics2D.Raycast(_groundChecker.position, Vector2.down, _rayLenght, _layerMask);
+            
+            if (hit)
             {
-                transform.position = Vector3.MoveTowards(transform.position, transform.position + direction, distance);
+                Jump();
             }
         }
 
         private void Jump()
         {
-            if (_canJumping == false)
-            {
-                Debug.Log("can not jump");
-                return;
-            }
-            
-            if (Input.GetKey(KeyCode.Space))
-            {
-                _rigidbody.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
-                _canJumping = false;
-            }
-        }
-        
-        public void OnGround()
-        {
-            _canJumping = true;
-        }
-
-        private Vector3 GetDirection()
-        {
-            float inputDirectionValue = Input.GetAxis(MovingAxisName);
-
-            Vector3 direction = inputDirectionValue switch
-            {
-                > 0 => Vector3.right,
-                < 0 => Vector3.left,
-                _ => Vector3.zero
-            };
-
-            return direction;
+            _rigidbody.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
         }
     }
 }
