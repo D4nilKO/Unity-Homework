403d6c861c3387ba12aab916948395996ab5d8f6
Даниил Понкратенко <ponkratenko.danil@mail.ru>
2023-12-08 23:08:31 +0300

    Add Full Animations

--- 2D Homework/Assets/Tasks/Platformer/Scripts/PlayerMovement.cs
+++ 2D Homework/Assets/Tasks/Platformer/Scripts/PlayerMovement.cs
@@ -1,25 +1,36 @@
+using System;
 using UnityEngine;
 
 namespace Tasks.Platformer.Scripts
 {
     [RequireComponent(typeof(Rigidbody2D))]
+    [RequireComponent(typeof(Animator))]
+    [RequireComponent(typeof(SpriteRenderer))]
     public class PlayerMovement : MonoBehaviour
     {
         private const string MovingAxisName = "Horizontal";
-        
-[SerializeField] private Transform _groundChecker;
+
+        public readonly int Speed = Animator.StringToHash(nameof(Speed));
+        public readonly int IsJumping = Animator.StringToHash(nameof(IsJumping));
+
+        [SerializeField] private Transform _groundChecker;
         [SerializeField] private float _speed = 3f;
         [SerializeField] private float _jumpForce = 15f;
 
-        [SerializeField] private LayerMask _layerMask;
-
+        [SerializeField] private LayerMask _groundLayerMask;
+        private float _rayLenght = 0.1f;
         private Rigidbody2D _rigidbody;
 
-        private float _rayLenght = 0.1f;
+        private Animator _animator;
+        private SpriteRenderer _spriteRenderer;
+
+        [SerializeField] private bool _isJumping;
 
         private void Start()
         {
             _rigidbody = GetComponent<Rigidbody2D>();
+            _animator = GetComponent<Animator>();
+            _spriteRenderer = GetComponent<SpriteRenderer>();
         }
 
         private void Update()
@@ -28,7 +39,7 @@ namespace Tasks.Platformer.Scripts
 
             if (Input.GetKeyDown(KeyCode.Space))
             {
-                GroundChecking();
+                TryJump();
             }
         }
 
@@ -36,15 +47,48 @@ namespace Tasks.Platformer.Scripts
         {
             float directionX = Input.GetAxis(MovingAxisName);
             _rigidbody.velocity = new Vector2(directionX * _speed, _rigidbody.velocity.y);
+
+            switch (directionX)
+            {
+                case < 0:
+                    _spriteRenderer.flipX = true;
+                    break;
+                case > 0:
+                    _spriteRenderer.flipX = false;
+                    break;
+            }
+
+            _animator.SetFloat(Speed, Mathf.Abs(directionX));
         }
 
-        private void GroundChecking()
+        private void OnCollisionEnter2D(Collision2D col)
         {
-            RaycastHit2D hit = Physics2D.Raycast(_groundChecker.position, Vector2.down, _rayLenght, _layerMask);
-            
+            if ((_groundLayerMask.value & (1 << col.gameObject.layer)) != 0)
+            {
+                _isJumping = false;
+                _animator.SetBool(IsJumping, _isJumping);
+            }
+        }
+
+        private void OnCollisionExit2D(Collision2D other)
+        {
+            if ((_groundLayerMask.value & (1 << other.gameObject.layer)) != 0)
+            {
+                _isJumping = true;
+                _animator.SetBool(IsJumping, _isJumping);
+            }
+        }
+
+        private void TryJump()
+        {
+            RaycastHit2D hit = Physics2D.Raycast(_groundChecker.position, Vector2.down, _rayLenght, _groundLayerMask);
+
             if (hit)
             {
                 Jump();
+
+                _isJumping = true;
+                _animator.SetBool(IsJumping, _isJumping);
             }
         }
 
