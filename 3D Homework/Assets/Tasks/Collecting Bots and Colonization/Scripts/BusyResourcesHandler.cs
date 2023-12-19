3b658913d84fcd9085d3ca6fc96557247a1624e8
Даниил Понкратенко <ponkratenko.danil@mail.ru>
2023-12-19 21:41:23 +0300

    Add new class

---
+++ 3D Homework/Assets/Tasks/Collecting Bots and Colonization/Scripts/BusyResourcesHandler.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Tasks.Collecting_Bots_and_Colonization.Scripts
+{
+    public class BusyResourcesHandler : MonoBehaviour
+    {
+        private List<Resource> _busyResources = new();
+
+        public void AddResource(Resource resource)
+        {
+            _busyResources.Add(resource);
+        }
+        
+        
+    }
+}
